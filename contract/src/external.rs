611a4de995fde8462491a682105b1448e11612f6
edsonalcala <edson_alcala@msn.com>
2025-06-19 09:35:14 +0100

    added interface

--- contract/src/external.rs
+++ contract/src/external.rs
@@ -1,8 +1,22 @@
-use near_sdk::ext_contract;
+use near_sdk::{ext_contract, serde::Serialize};
 use omni_transaction::evm::EVMTransaction;
+
+#[derive(Debug, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct SignRequest {
+    pub payload: [u8; 32],
+    pub path: String,
+    pub key_version: u32,
+}
+
+#[allow(dead_code)]
+#[ext_contract(mpc_contract)]
+trait MPCContract {
+    fn sign(&self, request: SignRequest);
+}
 
 #[allow(dead_code)]
 #[ext_contract(this_contract)]
 trait ThisContract {
-    fn callback(&self, ethereum_tx: EVMTransaction);
+    fn sign_callback(&self, nonce: u64, tx_type: u8, ethereum_tx: EVMTransaction) -> Vec<u8>;
 }
