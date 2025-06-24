eec684543d2712b66eb26c52cda323883d8a90c3
edsonalcala <edson_alcala@msn.com>
2025-06-24 13:15:58 +0200

    update prepaid gas

--- contract/src/constants.rs
+++ contract/src/constants.rs
@@ -4,5 +4,5 @@ pub const MPC_CONTRACT_ACCOUNT_ID: &str = "v1.signer-prod.testnet"; // PRODUCTIO
 pub const GAS: Gas = Gas::from_tgas(50);
 pub const PATH: &str = "ethereum-1";
 pub const KEY_VERSION: u32 = 0;
-pub const CALLBACK_GAS: Gas = Gas::from_tgas(30);
+pub const CALLBACK_GAS: Gas = Gas::from_tgas(90);
 pub const AAVE_LENDING_POOL_ADDRESS: &str = "0x7d2768de84f9a91b2c744cf0f0865d2e4b30f4bf"; // Mainnet Aave Lending Pool address
