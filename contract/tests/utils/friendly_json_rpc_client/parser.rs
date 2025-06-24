05457d91725026134810f7ae9be7d628f5ba2a05
edsonalcala <edson_alcala@msn.com>
2025-06-24 23:19:17 +0200

    added parser for vector of vectors

--- contract/tests/utils/friendly_json_rpc_client/parser.rs
+++ contract/tests/utils/friendly_json_rpc_client/parser.rs
@@ -27,3 +27,26 @@ impl ParseResult for String {
         Ok(result_str)
     }
 }
+
+impl ParseResult for Vec<Vec<u8>> {
+    fn parse(result_str: String) -> Result<Self, Box<dyn Error>> {
+        let cleaned = result_str.trim_matches(|c| c == '[' || c == ']');
+
+        // Split into inner arrays
+        let inner_strs = cleaned
+            .split("],[")
+            .map(|s| s.trim_matches(|c| c == '[' || c == ']'));
+
+        let mut result = Vec::new();
+
+        for inner in inner_strs {
+            let vec = inner
+                .split(',')
+                .map(|s| s.trim().parse::<u8>())
+                .collect::<Result<Vec<u8>, _>>()?;
+            result.push(vec);
+        }
+
+        Ok(result)
+    }
+}
