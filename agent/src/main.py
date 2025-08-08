0ae63ec7e5f93dd23c69d4698a7b075ec42c86e1
edsonalcala <edson_alcala@msn.com>
2025-08-08 13:56:45 +0200

    wip: logic to get allocations

--- agent/src/main.py
+++ agent/src/main.py
@@ -11,8 +11,9 @@ from near_omni_client.wallets import MPCWallet
 from near_omni_client.wallets.near_wallet import NearWallet
 from near_omni_client.crypto.keypair import KeyPair
 from near_omni_client.providers.near import NearFactoryProvider
-
-from utils import parse_chain_balances, parse_chain_configs, parse_u32_result, to_usdc_units
+from near_omni_client.json_rpc.client import NearClient
+from vault_abi import get_vault_abi
+from utils import from_chain_id_to_network, parse_chain_balances, parse_chain_configs, parse_u32_result, to_usdc_units
 from optimizer_data_fetcher import get_extra_data_for_optimization
 from optimizer import optimize_chain_allocation_with_direction
 from rebalancer import compute_rebalance_operations, execute_all_rebalances
@@ -31,9 +32,9 @@ async def main():
     one_time_signer_account_id = os.getenv("ONE_TIME_SIGNER_ACCOUNT_ID", "your_account_id_here")
     one_time_signer_public_key = os.getenv("ONE_TIME_SIGNER_PUBLIC_KEY", "your_public_key_here")
 
-    print("one_time_signer_private_key:", one_time_signer_private_key)
-    print("one_time_signer_account_id:", one_time_signer_account_id)
-    print("one_time_signer_public_key:", one_time_signer_public_key)
+    # print("one_time_signer_private_key:", one_time_signer_private_key)
+    # print("one_time_signer_account_id:", one_time_signer_account_id)
+    # print("one_time_signer_public_key:", one_time_signer_public_key)
 
     near_network = Network.parse(near_network)
     near_factory_provider = NearFactoryProvider()
@@ -46,7 +47,7 @@ async def main():
     )
     configs = parse_chain_configs(chain_config_raw)
     
-    print("Parsed Chain Configs:", configs)
+    # print("Parsed Chain Configs:", configs)
 
     source_chain_raw = await near_client.call_contract(
         contract_id=contract_id,
@@ -55,7 +56,7 @@ async def main():
     )
     source_chain_id = parse_u32_result(source_chain_raw)
 
-    print("Source Chain ID:", source_chain_id)
+    # print("Source Chain ID:", source_chain_id)
 
     current_allocations_raw = await near_client.call_contract(
         contract_id=contract_id,
@@ -66,6 +67,14 @@ async def main():
     current_allocations = parse_chain_balances(current_allocations_raw)
     print("Current Allocations:", current_allocations)
     
+    source_chain_config = configs.get(source_chain_id, None)
+    if not source_chain_config:
+        raise ValueError(f"Source chain config for chain ID {source_chain_id} not found in configs.")
+
+    vault_address = source_chain_config["rebalancer"]["vault_address"]
+    if not vault_address:
+        raise ValueError(f"Vault address for source chain {source_chain_id} not found in configs.")
+
     mpc_wallet = MPCWallet(
         path=PATH,
         account_id=contract_id, # The account ID of the contract on NEAR
@@ -74,6 +83,18 @@ async def main():
         supported_networks=[Network.OPTIMISM_SEPOLIA, Network.ARBITRUM_SEPOLIA],
     )
 
+    if all(v == 0 for v in current_allocations.values()):
+        print("⚠️ No prior rebalance detected. Fetching totalAssets from source chain...")
+        
+        web3 = mpc_wallet.get_web3(from_chain_id_to_network(source_chain_id))
+        contract = web3.eth.contract(address=vault_address, abi=get_vault_abi())
+        total_assets_under_management = contract.functions.totalAssets().call()
+        print(f"🔁 Using Total Assets Under Management from source chain ({source_chain_id}): {total_assets_under_management}")
+
+        current_allocations[source_chain_id] = total_assets_under_management
+
+    print("Current Allocations after fetching totalAssets:", current_allocations)
+    
     override_rates= os.getenv("OVERRIDE_INTEREST_RATES", "{}")
     try:
         override_interest_rates_raw = json.loads(override_rates)
@@ -81,9 +102,9 @@ async def main():
     except json.JSONDecodeError:
         raise ValueError("OVERRIDE_INTEREST_RATES must be a valid JSON dictionary.")
 
-    print("Override Interest Rates:", override_interest_rates)
+    # print("Override Interest Rates:", override_interest_rates)
 
-    extra_data_for_optimization = await get_extra_data_for_optimization(mpc_wallet, current_allocations, configs, source_chain_id, override_interest_rates)
+    extra_data_for_optimization = await get_extra_data_for_optimization(total_assets_under_management, mpc_wallet, current_allocations, configs, override_interest_rates)
 
     optimized_allocations = optimize_chain_allocation_with_direction(data=extra_data_for_optimization)
 
@@ -108,8 +129,9 @@ async def main():
         rebalance_operations=rebalance_operations,
         near_client=near_client,
         near_wallet=near_wallet,
-        AGENT_ADDRESS=PATH,
-        max_fee=to_usdc_units(max_bridge_fee),
+        near_contract_id=contract_id,
+        agent_address="0x23BF95De9F90338F973056351C8Cd2CB78cbe52f", # TODO: obtain the agent address
+        max_bridge_fee=to_usdc_units(max_bridge_fee),
         min_finality_threshold=min_bridge_finality_threshold,
     )
     print("Rebalance operations computed successfully.")
