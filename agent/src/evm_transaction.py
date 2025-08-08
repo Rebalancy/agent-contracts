69fea809cc8362d8879b4c991551d3de820a2735
edsonalcala <edson_alcala@msn.com>
2025-08-08 13:55:03 +0200

    added to dic to evm transaction

--- agent/src/evm_transaction.py
+++ agent/src/evm_transaction.py
@@ -13,10 +13,23 @@ class EVMTransaction:
     input: List[int]
     access_list: List[dict]
 
+    def to_dict(self):
+        return {
+            "chain_id": self.chain_id,
+            "nonce": self.nonce,
+            "gas_limit": self.gas_limit,
+            "max_fee_per_gas": self.max_fee_per_gas,
+            "max_priority_fee_per_gas": self.max_priority_fee_per_gas,
+            "to": self.to,
+            "value": self.value,
+            "input": self.input,
+            "access_list": self.access_list,
+        }
+
 def get_empty_tx_for_chain(chain_id: int) -> EVMTransaction:
     return EVMTransaction(
         chain_id=chain_id,
-        nonce=None,
+        nonce=0,
         gas_limit=0,
         max_fee_per_gas=0,
         max_priority_fee_per_gas=0,
