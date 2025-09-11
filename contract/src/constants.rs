f90cab78638355763c030ed0aaad80554d6d82dd
edsonalcala <edson_alcala@msn.com>
2025-09-11 19:54:04 +0300

    wip: provisional timeout

--- contract/src/constants.rs
+++ contract/src/constants.rs
@@ -5,5 +5,5 @@ pub const MPC_CONTRACT_ACCOUNT_ID_TESTNET: &str = "v1.signer-prod.testnet";
 pub const PATH: &str = "ethereum-1";
 pub const KEY_VERSION: u32 = 0;
 pub const CALLBACK_GAS: Gas = Gas::from_tgas(50);
-pub const OPERATIONS_TIMEOUT: u64 = 5 * 60 * 1000; // 5 minutes timeout
+pub const OPERATIONS_TIMEOUT: u64 = 1 * 60 * 1000; // 5 minutes timeout
 pub const ATTACHED_DEPOSIT: NearToken = NearToken::from_yoctonear(500000000000000000000000);
