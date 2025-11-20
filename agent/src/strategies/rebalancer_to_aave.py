2cef72cacc2a4215e3fc197cb10e7460e00cb2e2
edsonalcala <edson_alcala@msn.com>
2025-11-20 18:35:29 +0100

    fixed assertion and approval

--- agent/src/strategies/rebalancer_to_aave.py
+++ agent/src/strategies/rebalancer_to_aave.py
@@ -144,11 +144,12 @@ class RebalancerToAave(Strategy):
         Assert.usdc_agent_balance_is_at_least(web3_instance_destination_chain, usdc_address_on_destination_chain, expected_balance=amount)
 
         # Step 7: Approve USDC before supply on destination chain
-        lending_pool_address = self.remote_config[to_chain_id]["aave"]["lending_pool_address"] # the lending pool is the spender
-        spender = lending_pool_address
+        aave_lending_pool_address = self.remote_config[to_chain_id]["aave"]["lending_pool_address"] # the lending pool is the spender
+        spender = aave_lending_pool_address
         approve_usdc_aave_payload = await self.rebalancer_contract.build_and_sign_aave_approve_before_supply_tx(
-            to_chain_id=to_chain_id,amount=amount, 
-            spender=lending_pool_address, 
+            to_chain_id=to_chain_id,
+            amount=int(amount * 2), 
+            spender=aave_lending_pool_address, 
             to=usdc_address_on_destination_chain
         )
         try:
@@ -163,18 +164,19 @@ class RebalancerToAave(Strategy):
         asset = self.remote_config[to_chain_id]["aave"]["asset"]
         on_behalf_of = self.agent_address
         referral_code = self.remote_config[to_chain_id]["aave"]["referral_code"]
-        aave_lending_pool_address = self.remote_config[to_chain_id]["aave"]["lending_pool_address"]
         aave_supply_payload = await self.rebalancer_contract.build_and_sign_aave_supply_tx(to_chain_id=to_chain_id, asset=asset, amount=amount, on_behalf_of=on_behalf_of, referral_code=referral_code, to=aave_lending_pool_address)
 
         a_token_address = LendingPool.get_atoken_address(web3_instance_destination_chain, aave_lending_pool_address, asset)
         a_token_balance_before = BalanceHelper.get_atoken_agent_balance(web3_instance_destination_chain, a_token_address)
-        
+
         try:
             broadcast(web3_instance_destination_chain, aave_supply_payload)
         except Exception as e:
             print(f"Error broadcasting supply transaction: {e}")
             return
         
+        time.sleep(3)
+
         # Step 8: Assert aToken balance is correct after supply and also considers previous balance
         Assert.atoken_agent_balance(web3_instance_destination_chain, a_token_address, expected_balance=amount + a_token_balance_before)
         
