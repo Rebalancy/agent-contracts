81c1fab06144246b6a4d756226fc88f059a1d01f
edsonalcala <edson_alcala@msn.com>
2025-11-26 19:48:41 +0100

    updated rebalancer to aave strategy

--- agent/src/strategies/rebalancer_to_aave.py
+++ agent/src/strategies/rebalancer_to_aave.py
@@ -5,8 +5,9 @@ from near_omni_client.providers.evm.alchemy_provider import AlchemyFactoryProvid
 from near_omni_client.adapters.cctp import AttestationService, FeeService
 
 from .strategy import Strategy
-from helpers import broadcast, Assert, LendingPool, BalanceHelper
+from helpers import broadcast, Assert, BalanceHelper
 from config import Config
+from adapters import LendingPool
 from adapters.rebalancer_contract import RebalancerContract
 from .steps.types import Flow
 from utils import from_chain_id_to_network
