c02443a729fda53b0068407ca73e3617e688cd58
edsonalcala <edson_alcala@msn.com>
2025-12-04 22:57:45 +0100

    fix withdraw from aave

--- agent/src/strategies/steps/p6_withdraw_from_aave.py
+++ agent/src/strategies/steps/p6_withdraw_from_aave.py
@@ -6,9 +6,16 @@ class WithdrawFromAave(Step):
     NAME = "WithdrawFromAave"
 
     async def run(self, ctx: StrategyContext) -> None:
-        payload = await ctx.rebalancer_contract.build_aave_withdraw_tx(
-            from_chain_id=ctx.from_chain_id,
-            amount=ctx.amount
+        asset = ctx.usdc_token_address_on_source_chain
+        on_behalf = ctx.agent_address
+        # @dev since we can only interact directly with an Aave Lending Pool contract in a NON-Source chain,
+        # it means that we are trying to move funds from the NON-Source chain, therefore the chain id is the from_chain_id
+        payload = await ctx.rebalancer_contract.build_and_sign_aave_withdraw_tx(
+            chain_id=ctx.from_chain_id,
+            asset=asset,
+            amount=ctx.amount,
+            on_behalf_of=on_behalf,
+            to=ctx.aave_lending_pool_address_on_source_chain
         )
         broadcast(ctx.web3_source, payload)
 
