8ebc30c24d3c257395c2bc17b97cb3678865adbb
edsonalcala <edson_alcala@msn.com>
2025-12-06 00:13:32 +0100

    added deposit into rebalancer except get cross chain balance

--- agent/src/strategies/steps/p7_deposit_into_rebalancer.py
+++ agent/src/strategies/steps/p7_deposit_into_rebalancer.py
@@ -1,12 +1,15 @@
 from helpers import broadcast
 from ..strategy_context import StrategyContext
+from .step import Step
 
-class DepositIntoRebalancer:
+class DepositIntoRebalancer(Step):
     NAME = "DepositIntoRebalancer"
 
     async def run(self, ctx: StrategyContext):
-        payload = await ctx.rebalancer_contract.build_rebalancer_deposit_tx(
-            to_chain_id=ctx.to_chain_id,
-            amount=ctx.amount
-        )
-        broadcast(ctx.web3_destination, payload)
+        print("Depositing into rebalancer...")
+        crosschain_balance = ctx.amount # TODO: Fix this with process to get the actual crosschain balance
+        deposit_payload = await ctx.rebalancer_contract.build_and_sign_return_funds_tx(to_chain_id=ctx.to_chain_id, cross_chain_a_token_balance=crosschain_balance,  amount=ctx.amount, to=ctx.vault_address)
+
+        broadcast(ctx.web3_destination, deposit_payload)
+
+        print("Deposit transaction broadcasted successfully!")
