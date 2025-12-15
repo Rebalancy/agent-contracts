15d30a4a5b4ec66fc5f20de07cd633e0a6b3d3c8
edsonalcala <edson_alcala@msn.com>
2025-12-15 23:56:23 +0100

    integrated allowance logic

--- agent/src/main.py
+++ agent/src/main.py
@@ -1,11 +1,13 @@
 import asyncio
 from dstack_sdk import DstackClient
 
+from utils import from_chain_id_to_network
 from config import Config
 from helpers import Assert, BalanceHelper
 from optimizer import get_extra_data_for_optimization, optimize_chain_allocation_with_direction
 from engine import build_context, StrategyManager, execute_all_rebalance_operations,compute_rebalance_operations, get_allocations
-
+from adapters import Vault, USDC
+from engine import EngineContext
 
 async def main():
     # Load configuration from environment variables
@@ -26,6 +28,11 @@ async def main():
 
     # Configure Strategies
     StrategyManager.configure(rebalancer_contract=context.rebalancer_contract, evm_factory_provider = context.evm_factory_provider, vault_address=vault_address, config=config, remote_config=context.remote_configs, agent_address=agent_evm_address)
+
+    # Check allowances and execute transactions if required
+    max_allowance = Vault(context.vault_address, context.source_network, context.evm_factory_provider).get_max_total_deposits()
+    print(f"Max allowance for vault {vault_address} on source chain: {max_allowance}")
+    await check_and_execute_allowances(context, max_allowance)
 
     current_allocations, total_assets_under_management = await get_allocations(context)
     
@@ -58,3 +65,33 @@ async def main():
 if __name__ == "__main__":
     asyncio.run(main())
 
+
+async def check_and_execute_allowances(context: EngineContext, max_allowance: int):
+    supported_chains = await context.rebalancer_contract.get_supported_chains()
+    
+    for chain_id in supported_chains:
+        network_id = from_chain_id_to_network(chain_id)
+        web3_instance = context.evm_factory_provider.get_provider(network_id)
+        aave_lending_pool_address = context.remote_configs[chain_id]["aave"]["lending_pool_address"]
+        usdc_token_address: str = context.remote_configs[chain_id]["aave"]["asset"]
+        messenger_address = context.remote_config[chain_id]["cctp"]["messenger_address"]
+        messenger_allowance = USDC.get_allowance(web3_instance=web3_instance, usdc_address=usdc_token_address, spender=messenger_address)
+
+        if messenger_allowance < max_allowance:
+            # we execute tx
+            pass
+
+        if chain_id == context.source_chain_id:
+            # TODO: revisar si necesito la address del owner
+            vault_allowance = USDC.get_allowance(web3_instance=web3_instance, usdc_address=usdc_token_address, spender=context.vault_address)
+            if vault_allowance < max_allowance:
+                # here we execute the allowance
+                pass     
+            pass 
+        else:
+            lending_pool_allowance = USDC.get_allowance(web3_instance=web3_instance, usdc_address=usdc_token_address, spender=aave_lending_pool_address)
+            if lending_pool_allowance < max_allowance:
+                # here we execute the allowance
+                pass
+            pass
+           
