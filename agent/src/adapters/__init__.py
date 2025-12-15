4e6935451f08416ea85d72b18f5c47093938b748
edsonalcala <edson_alcala@msn.com>
2025-12-15 23:56:38 +0100

    added usdc adapter

--- agent/src/adapters/__init__.py
+++ agent/src/adapters/__init__.py
@@ -1,10 +1,11 @@
 from .rebalancer_contract import RebalancerContract
 from .vault_contract import Vault
 from .lending_pool_contract import LendingPool
-
+from .usdc_contract import USDC
 
 __all__ = [
     "RebalancerContract",
     "Vault",
     "LendingPool",
+    "USDC"
 ]
---
+++ agent/src/adapters/usdc_contract.py
@@ -0,0 +1,14 @@
+from web3 import Web3
+from .abis import USDC_ABI
+
+# TODO: agregar usdc abi
+class USDC:
+    @staticmethod
+    def get_allowance(web3_instance: Web3, usdc_address: str, spender: str) -> str:
+        """Return the aToken address for a given asset."""
+        contract = web3_instance.eth.contract(address=usdc_address, abi=USDC_ABI)
+
+        # TODO: Review this
+        allowance = contract.functions.allowance(spender).call()       
+
+        return allowance
